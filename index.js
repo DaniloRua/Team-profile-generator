ebea1c20fefa846b897498f4b286e80c36427ae4
Danilopc <sr_danilo@hotmail.com>
2024-02-13 13:47:57 +0000

    init fuction + questions

--- index.js
+++ index.js
@@ -10,11 +10,64 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
+const teamMembers = [];
 
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
 
-const questions = () => {
-    return inquirer.createPromptModule([
-    
+
+function init() {
+  console.log(`    
+  =================
+  Adding employees to the team
+  =================
+  `);
+  inquirer.prompt([
+      {
+        type: "list",
+        name: "role",
+        message: "What is your role?",
+        choices: ["manager", "intern", "engineer"]
+      },
+      {
+        type: "input",
+        name: "name",
+        message: "What is the name?"
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the ID?"
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the email?"
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "What is the GitHub username?",
+        when: (answer) => answer.role === "engineer"
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "What is the school?",
+        when: (answer) => answer.role === "intern"
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the office number?",
+        when: (answer) => answer.role === "manager"
+      },
+      {
+        type: "list",
+        name: "addMember",
+        message: "Would you like to add more members on your team?",
+        choices: ["yes", "no"]
+      }
     ])
+
 }
+
+init();
